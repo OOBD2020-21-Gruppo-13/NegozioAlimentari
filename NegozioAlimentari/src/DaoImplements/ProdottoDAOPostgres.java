afa2584e68dc77c23da878ffec8fb8bd4cfbba59
Francesco Ciccarelli <francesco.ciccarelli2@studenti.unina.it>
2021-01-07 14:54:44 +0100

    Ho implementato la classe ProdottoDAO.

---
+++ NegozioAlimentari/src/DaoImplements/ProdottoDAOPostgres.java
@@ -0,0 +1,67 @@
+package DaoImplements;
+import java.sql.*;
+import java.util.ArrayList;
+import ClassiDB.Prodotto;
+import Dao.ProdottoDAO;
+
+public class ProdottoDAOPostgres implements ProdottoDAO{
+
+	Connection con=null;
+	ArrayList<Prodotto> Magazzino = new ArrayList<>();
+	
+public ProdottoDAOPostgres(Connection connection) { 
+	this.con=connection;
+   
+    try 
+    {										
+    PreparedStatement st = con.prepareStatement("SELECT * FROM prodotto");
+    ResultSet rs = st.executeQuery();
+
+    while(rs.next())
+    {
+    	String nome=rs.getString("nome");
+    	String tipo=rs.getString("Tipo");
+    	double prezzo=rs.getDouble("prezzo");
+    	int quantita=rs.getInt("quantitamag");
+    	int idProdotto=rs.getInt("idprodotto");
+    	Date dataScadenza=rs.getDate("datascadenza");
+    	Prodotto p = new Prodotto(nome,tipo,prezzo,quantita,idProdotto,dataScadenza);
+
+    	switch(tipo) 
+    	{
+			case "farinaceo","confezionato","uova":
+				Date DataProduzione = rs.getDate("dataproduzione");
+				p.setDataProduzione(DataProduzione);
+				Magazzino.add(p);
+				break;
+			case "frutta","verdura":
+				Date DataRaccolta = rs.getDate("dataraccolta");
+				p.setDataRaccolta(DataRaccolta);
+				Magazzino.add(p);
+				break;
+			case "latticino":
+				Date DataProduzione1 = rs.getDate("dataproduzione");
+				Date DataMungitura = rs.getDate("datamungitura");
+				p.setDataMungitura(DataMungitura);
+				p.setDataProduzione(DataProduzione1);
+				Magazzino.add(p);
+				break;
+    	}	
+    }
+    
+    rs.close();
+    st.close();
+   
+    }
+    catch(SQLException e) 
+    {
+        System.out.println("SQL Exception: \n"+e);
+    }
+	
+  }
+
+public ArrayList<Prodotto> getMagazzino() {
+	return Magazzino;
+}
+	
+}
--- NegozioAlimentari/src/Main/Starter.java
+++ NegozioAlimentari/src/Main/Starter.java
@@ -1,6 +1,5 @@
 package Main;
 import java.sql.*;
-
 import DaoImplements.*;
 import Database.DBConnection;
 
@@ -10,6 +9,7 @@ public class Starter {
     Connection connection = null;
     AcquistiDAOPostgres DAO1 = null;
     ClienteDAOPostgres DAO2 = null;
+    ProdottoDAOPostgres DAO3 = null;
 	
 	public Starter() throws SQLException 
 	{
@@ -17,6 +17,7 @@ public class Starter {
         connection = dbconn.getConnection();
         DAO1 = new AcquistiDAOPostgres(connection);
         DAO2 = new ClienteDAOPostgres(connection);
+        DAO3 = new ProdottoDAOPostgres(connection);
 	}
 	
 	public static void main(String[] args) throws SQLException 
@@ -25,8 +26,7 @@ public class Starter {
 		System.out.println("Hello Database");
 		System.out.println(s.DAO1.getAcquisti());
 		System.out.println(s.DAO2.getClienti());
+		System.out.println(s.DAO3.getMagazzino());
 	}
-
-
 	
 }
